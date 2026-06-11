763144d389becd4b1f942c801c26087aae031ee4
orderly-guoqing <guoqing@orderly.network>
2026-06-11 10:59:01 +0800

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -19,7 +19,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
   "VITE_ENABLED_MENUS": "Trading,Markets,Swap,Vaults,Points,Portfolio",
-  "VITE_CUSTOM_MENUS": "交易大赛, https://alpha.dashwin.org; AAA联合做市商, https://rshl.aaaclub.net; 预测Coming soon, https://prediction.dashwin.org; 红利基金Coming soon, https://dashwin.org",
+  "VITE_CUSTOM_MENUS": "交易大赛, https://alpha.dashwin.org; AAA联合做市商, https://rshl.aaaclub.net; 体育预测, https://predict.dashwin.org; 红利基金Coming soon, https://dashwin.org",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "",
